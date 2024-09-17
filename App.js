fc277e63dddce25a0a48eef955a63add6d17a207
Augusto <augustoortolan02@gmail.com>
2024-09-17 20:35:34 -0300

    Componentes e stilos

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.container}>
+        <Text style={[styles.texto, styles.texto2, { fontSize: 10 }]}>
+          Olá Mundo!
+        </Text>
+        <Button color={"red"} title="Botao" />
+        <ActivityIndicator size={"large"} color={"red"} />
+        <StatusBar style="auto" />
+      </View>
+      <View style={styles.container2}>
+        <Text style={styles.texto}>Olá Mundo!</Text>
+        <Button color={"red"} title="Botao" />
+        <ActivityIndicator size={"large"} color={"red"} />
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    flex: 0.8,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  container2: {
+    flex: 0.2,
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  texto: { fontSize: 50, fontWeight: "bold" },
+  texto2: {
+    color: "green",
+    fontSize: 80,
   },
 });
